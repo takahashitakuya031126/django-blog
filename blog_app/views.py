9ced96aa84d0f191f87cecf4a48cc567c0697287
EC2 Default User <ec2-user@ip-172-31-3-227.ap-northeast-1.compute.internal>
2020-06-16 08:26:43 +0000

    add filter

--- blog_app/views.py
+++ blog_app/views.py
@@ -12,17 +12,32 @@ from django.db.models import Q
 
 # Create your views here.
 
+def is_valid_q(q):
+    return q != '' and q is not None
+
 def index(request):
-    query = request.GET.get('q')
-    if query:
-        posts = Post.objects.all().order_by('-created_at')
-        posts = posts.filter(
-        Q(title__icontains=query)|
-        Q(user__username__icontains=query)
-        ).distinct()
-    else:
-        posts = Post.objects.all().order_by('-created_at')  
-    return render(request, 'blog_app/index.html', {'posts': posts, 'query': query,})
+    posts = Post.objects.all().order_by('-created_at')
+    title_or_user = request.GET.get('title_or_user')
+    date_min = request.GET.get('date_min')
+    date_max = request.GET.get('date_max')
+    tag = request.GET.get('tag')
+
+    if is_valid_q(title_or_user):
+        posts = posts.filter(Q(title__icontains=title_or_user)
+                        | Q(user__username__icontains=title_or_user)
+                        ).distinct()
+
+    if is_valid_q(date_min):
+        posts = posts.filter(created_at__gte=date_min)
+
+    if is_valid_q(date_max):
+        posts = posts.filter(created_at__lt=date_max)
+
+    if is_valid_q(tag) and tag != 'タグを選択...':
+        posts = posts.filter(tag__tag=tag)
+   
+    return render(request, 'blog_app/index.html', 
+    {'posts': posts, 'title_or_user': title_or_user , 'date_min': date_min, 'date_max': date_max ,'tag': tag})
 
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
