41a1cb1a96695d03f07680222373f6cf99ac735e
EC2 Default User <ec2-user@ip-172-31-3-227.ap-northeast-1.compute.internal>
2020-06-16 02:44:18 +0000

    create detail

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'blog_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:post_id>/', views.detail, name='detail'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Tag
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog_app/index.html')
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog_app/index.html', {'posts': posts})
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'blog_app/detail.html', {'post': post})
