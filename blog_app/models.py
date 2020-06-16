1f6dc6ef69be12dc86aa5caac28db58e25fc4fd3
EC2 Default User <ec2-user@ip-172-31-3-227.ap-northeast-1.compute.internal>
2020-06-16 02:21:46 +0000

    create post table

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Tag(models.Model):
+    tag = models.CharField('タグ名', max_length=50)
+    def __str__(self):
+        return self.tag
+
+class Post(models.Model):
+    title = models.CharField('タイトル', max_length=35)
+    text = models.TextField('本文')
+    image = models.ImageField('画像', upload_to = 'images', blank=True)
+    created_at = models.DateTimeField('投稿日', default=timezone.now)
+    tag = models.ForeignKey(Tag, verbose_name = 'タグ', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
