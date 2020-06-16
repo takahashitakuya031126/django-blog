9514e085fb34adef5add41feb1da69bac978b640
EC2 Default User <ec2-user@ip-172-31-3-227.ap-northeast-1.compute.internal>
2020-06-16 02:56:22 +0000

    add post,edit,delete functions

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,4 +5,7 @@ app_name = 'blog_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:post_id>/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    path('edit/<int:post_id>/', views.edit, name='edit'),
+    path('delete/<int:post_id>/', views.delete, name='delete'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Tag
+from .forms import PostAddForm
 
 # Create your views here.
 
@@ -13,3 +14,31 @@ def index(request):
 def detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     return render(request, 'blog_app/detail.html', {'post': post})
+
+def add(request):
+    if request.method == "POST":
+        form = PostAddForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('blog_app:index')
+    else:
+       form = PostAddForm()
+    return render(request, 'blog_app/add.html', {'form': form})
+
+def edit(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostAddForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_app:detail', post_id=post.id)
+    else:
+        form = PostAddForm(instance=post)
+    return render(request, 'blog_app/edit.html', {'form': form, 'post':post })
+
+def delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('blog_app:index')
